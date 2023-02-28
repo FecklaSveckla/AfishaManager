fe8c53608f91fb2ac7459782999c293dcdea21d9
Beloglazova Olga <beloglazovaom@yandex.ru>
2023-02-28 15:20:13 +0300

    Add test

---
+++ src/main/java/ru/netology/Domain/Movie.java
@@ -0,0 +1,18 @@
+package ru.netology.Domain;
+
+
+public class Movie {
+    private int id;
+    private String name;
+    private String imageUrl;
+    private String genre;
+
+
+    public Movie(int id, String name, String imageUrl, String genre) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.genre = genre;
+    }
+}
+
